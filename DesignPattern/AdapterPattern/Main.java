26c4dd672cf975d0c7254249c36a0e703bf8858a
PS2207 <pragyasinha2207@gmail.com>
2023-08-15 14:10:47 +0530

    Update Main.java

--- DesignPattern/AdapterPattern/Main.java
+++ DesignPattern/AdapterPattern/Main.java
@@ -2,6 +2,6 @@ package DesignPattern.AdapterPattern;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello adapter");
+        System.out.println("hello adapterpattern");
     }
 }
