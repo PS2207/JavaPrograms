4bf26caed4a58eb9ef4f74b7356fb56243a4dd10
PS2207 <pragyasinha2207@gmail.com>
2023-08-16 16:41:28 +0530

    Update Main.java

--- DesignPattern/SingletonPattern/Main.java
+++ DesignPattern/SingletonPattern/Main.java
@@ -2,6 +2,6 @@ package DesignPattern.SingletonPattern;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello singleton pattern");
+        System.out.println("Singleton design pattern works here...");
     }
 }
