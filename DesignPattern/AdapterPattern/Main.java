c68dd015ce1cb3c3d5a5f11dd4710127fd0acca4
PS2207 <pragyasinha2207@gmail.com>
2023-08-18 11:09:02 +0530

    Update Main.java

--- DesignPattern/AdapterPattern/Main.java
+++ DesignPattern/AdapterPattern/Main.java
@@ -1,7 +1,13 @@
 package DesignPattern.AdapterPattern;
 
 public class Main {
+    // Adapter Pattern- Charge apple phone with an android charger using an AppleConnecter.
     public static void main(String[] args) {
-        System.out.println("hello adapterpattern");
+        System.out.println("Adapter pattern works here!!");
+        // AppleCharger charger =new ChargerXYZ(); //iphone charger
+        AppleCharger charger =new AppleConnector(new DKCharger()); //Android charger+apple connector
+        Iphone iPhone = new Iphone(charger);
+        iPhone.chargeIphone();
+       
     }
 }
