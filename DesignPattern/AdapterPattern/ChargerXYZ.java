e842356cdfa0faa8df40dff469a06bb7a1e4d999
PS2207 <pragyasinha2207@gmail.com>
2023-08-18 11:09:26 +0530

    Create ChargerXYZ.java

---
+++ DesignPattern/AdapterPattern/ChargerXYZ.java
@@ -0,0 +1,11 @@
+package DesignPattern.AdapterPattern;
+
+public class ChargerXYZ implements AppleCharger {
+
+    @Override
+    public void chargePhone() {
+       
+       System.out.println("Your Iphone is charging");
+    }
+
+}
