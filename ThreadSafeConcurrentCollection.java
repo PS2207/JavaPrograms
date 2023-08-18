361b4570508075bb06fa2be8f69d3fc440d2693f
PS2207 <pragyasinha2207@gmail.com>
2023-08-18 11:09:12 +0530

    Create ThreadSafeConcurrentCollection.java

---
+++ ThreadSafeConcurrentCollection.java
@@ -0,0 +1,46 @@
+
+// import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class ThreadSafeConcurrentCollection {
+    public static void main(String[] args) {
+        List<String> list = new CopyOnWriteArrayList<>();
+        list.add("java");
+        list.add("python");
+        list.add("javascript");
+        list.add("ruby");
+        list.add("nodejs");
+        list.add("pearl");
+
+        Runnable t1 = () -> {
+            for (String str : list) {
+                System.out.println(str);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+
+                    e.printStackTrace();
+                }
+            }
+        };
+        // both way we can make thread
+        // Runnable t2=new Runnable(){
+        //     @Override
+        //     public void run(){
+        //            list.add("hello");
+        //     list.add("hi");
+        //     }
+        // };
+        // creating thread using lamda
+        Runnable t2 = () -> {
+            list.add("hello");
+            list.add("hi");
+        };
+           
+        // starting thread
+        new Thread(t2).start();
+        new Thread(t1).start();
+
+    }
+}
