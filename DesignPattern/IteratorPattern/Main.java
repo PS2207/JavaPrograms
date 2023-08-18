a557183f335a527b3301b1c6e3ad18d88ae560cb
PS2207 <pragyasinha2207@gmail.com>
2023-08-18 15:03:20 +0530

    Update Main.java

--- DesignPattern/IteratorPattern/Main.java
+++ DesignPattern/IteratorPattern/Main.java
@@ -2,6 +2,20 @@ package DesignPattern.IteratorPattern;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello iterator");
+     // Iterator design pattern provides a way to access the elements of an object without exposing its underlying implementation.
+    //  we want to traverse all the users of UserManagement without knowing its implementation.
+     UserManagement userManagement=new UserManagement();
+     userManagement.addUser(new User("Pragya", "101"));
+     userManagement.addUser(new User("Komal", "102"));
+     userManagement.addUser(new User("Priya", "103"));
+
+     MyIterator itr= userManagement.getIterator();
+     while(itr.hasNext()){
+        User user =(User) itr.next();
+        System.out.println(user);
+         System.out.println(user.getName()); 
+     }
+
+
     }
 }
