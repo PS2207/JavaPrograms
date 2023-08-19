34452a96db2b75d4fc9ed3af9f78a0c1ae28b9db
PS2207 <pragyasinha2207@gmail.com>
2023-08-19 16:20:28 +0530

    Create Employee.java

---
+++ FileHandling/Employee.java
@@ -0,0 +1,28 @@
+package FileHandling;
+
+public class Employee {
+  
+private int empId;
+   private String empName;
+   private int salary;
+@Override
+public String toString() {
+    return "Employee [empId=" + empId + ", empName=" + empName + ", salary=" + salary + "]";
+}
+public int getEmpId() {
+    return empId;
+}
+public String getEmpName() {
+    return empName;
+}
+public int getSalary() {
+    return salary;
+}
+public Employee(int empId, String empName, int salary) {
+    this.empId = empId;
+    this.empName = empName;
+    this.salary = salary;
+}
+
+   
+}
