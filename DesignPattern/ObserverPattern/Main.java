89dc101c490fa78acf3044a0a92496d7c659bb06
PS2207 <pragyasinha2207@gmail.com>
2023-08-21 16:00:11 +0530

    Update Main.java

--- DesignPattern/ObserverPattern/Main.java
+++ DesignPattern/ObserverPattern/Main.java
@@ -1,7 +1,15 @@
 package DesignPattern.ObserverPattern;
 
 public class Main {
-   public static void main(String[] args) {
-    
-   } 
+    public static void main(String[] args) {
+        // YoutubeChannel channel = new YoutubeChannel();
+        // Subscriber pragya = new Subscriber();
+        Subject channel = new YoutubeChannel();
+        Observer pragya = new Subscriber("Pragya");
+        Observer priya = new Subscriber("Priya");
+        channel.subscribe(pragya);
+        channel.subscribe(priya);
+        channel.newVideoUploaded("Learn design pattern");
+        channel.newVideoUploaded("Learn Java course");
+    }
 }
