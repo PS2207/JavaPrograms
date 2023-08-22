2c130c83f6f55119dee0fa096fc4a878cd3ea2a4
PS2207 <pragyasinha2207@gmail.com>
2023-08-22 16:39:02 +0530

    Create ManagerFactory.java

---
+++ DesignPattern/AbstractFactoryPattern/ManagerFactory.java
@@ -0,0 +1,11 @@
+package DesignPattern.AbstractFactoryPattern;
+
+public class ManagerFactory extends EmployeeAbstractFactory{
+
+    @Override
+    public Employee createEmployee() {
+       return new Manager();
+     
+    }
+    
+}
