6e52cddb997b3ca0ad89a0056a4610844a0e8f79
PS2207 <pragyasinha2207@gmail.com>
2023-08-22 16:38:58 +0530

    Create WebDeveloper.java

---
+++ DesignPattern/AbstractFactoryPattern/WebDeveloper.java
@@ -0,0 +1,16 @@
+package DesignPattern.AbstractFactoryPattern;
+
+public class WebDeveloper implements Employee{
+
+    @Override
+    public int salary() {
+        return 40000;
+    }
+
+    @Override
+    public String name() {
+        System.out.println("I am a Web developer");
+        return "WEB DEVELOPER";
+    }
+    
+}
