fe21711697353188a475c387661bc1dd789f5270
PS2207 <pragyasinha2207@gmail.com>
2023-08-26 15:48:53 +0530

    Create Count_Space_LowerCase_UpperCase_OtherChars.java

---
+++ Count_Space_LowerCase_UpperCase_OtherChars.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Count_Space_LowerCase_UpperCase_OtherChars {
+    public static void main(String[] args) {
+        System.out.println("Enter a string: ");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println("\nEntered String is " + str);
+        new Count(sc, str);
+    }
+}
+
+class Count {
+    private final String str;
+    private final Scanner sc;
+    private int spaceCount = 0;
+    private int lowerCount = 0;
+    private int upperCount = 0;
+    private int digitCount = 0;
+    private int otherCount = 0;
+
+    public Count(Scanner sc, String str) {
+        this.str = str;
+        this.sc = sc;
+        countChar();
+    }
+
+    public void countChar() {
+        // Way1- count Uppercase,Lowercase, sapce & so on using ASCII value
+        for (int i = 0; i <= str.length() - 1; i++) {
+            char ch = str.charAt(i);
+            if (ch == 32) {
+                spaceCount++;
+            } else if (ch >= 97 && ch <= 122) {
+                lowerCount++;
+            } else if (ch >= 65 && ch <= 90) {
+                upperCount++;
+            } else if (ch >= 48 && ch <= 57) {
+                digitCount++;
+            } else {
+                otherCount++;
+            }
+        }
+        System.out.println("Space count: " + spaceCount);
+        System.out.println("Lower count: " + lowerCount);
+        System.out.println("Upper count: " + upperCount);
+        System.out.println("Digit count: " + digitCount);
+        System.out.println("Other count: " + otherCount);
+    }
+
+}
+// ____________OUTPUT__________
+// Enter a string:
+// Hello World !!
+
+// Entered String is hello
+// Space count: 3
+// Lower count: 8
+// Upper count: 2
+// Digit count: 0
+// Other count: 2
+// _____________________________
